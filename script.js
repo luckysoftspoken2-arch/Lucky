f8e368e8f57a25732e3df23e4364c1bad7ef71c0
luckysoftspoken2-arch <luckysoftspoken2@gmail.com>
2025-10-21 13:24:29 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+const tombolSapa=document.getElementById(`sapaButton`);
+// 2. Tambahkan `event Listenter` unutuk aksi `click`
+tombolsapa.addEventListener(`click`, function() {
+     // 3. Jalankan kode ini ketika tombol di-klik
+    alert(`Halo! Terimaksi kasih sudah berkunjung!`);
+});
